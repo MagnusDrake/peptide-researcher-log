26e0ea4efeae00b93525f02ad45529511c237f9d
Jose Luis Ojeda De Leon <proxtend@gmail.com>
2026-08-27 20:26:48 -0500

    fix: make metallic flakes monochrome to prevent rainbow discoloration of background blobs

--- src/App.tsx
+++ src/App.tsx
@@ -507,7 +507,7 @@ export function App() {
           style={{
             zIndex: 0,
             opacity: flakeOpacity,
-            backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='flake'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='2.0' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23flake)' opacity='1'/%3E%3C/svg%3E")`,
+            backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='flake'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='2.0' numOctaves='3' stitchTiles='stitch'/%3E%3CfeColorMatrix type='saturate' values='0'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23flake)' opacity='1'/%3E%3C/svg%3E")`,
             backgroundRepeat: 'repeat',
             backgroundSize: '150px 150px'
           }}
